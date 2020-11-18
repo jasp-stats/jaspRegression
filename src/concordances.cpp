6daae1bbe06bfa9b2fa66f6aafc743b0e76308dc
Kucharssim <kucharssim@gmail.com>
2020-11-18 12:09:13 +0100

    tabs instead of spaces in .cpp

--- src/concordances.cpp
+++ src/concordances.cpp
@@ -2,26 +2,26 @@
 using namespace Rcpp;
 
 int sign(const double& x){
-  return x == 0 ? 0 : x > 0 ? 1 : -1;
+	return x == 0 ? 0 : x > 0 ? 1 : -1;
 }
 
 // Defined in Hollander, Wolfe & Chicken, Nonparametric Statistical Methods, 3ed., eq. (8.5), p. 394
 int Q(const double& a, const double& b, const double& c, const double& d) {
-  return sign((d-b)*(c-a));
+	return sign((d-b)*(c-a));
 }
 
 // Defined in Hollander, Wolfe & Chicken, Nonparametric Statistical Methods, 3ed., eq. (8.37), p. 415
 // [[Rcpp::export]]
 IntegerVector concordanceVector_cpp(const NumericVector& x, const NumericVector& y){
-  int n = x.size();
-  IntegerVector out(n);
+	int n = x.size();
+	IntegerVector out(n);
 
-  for(int i = 0; i < n; i++){
-    out[i] = 0;
-    for(int t = 0; t < n; t++){
-      if(i != t) out[i] += Q(x[i], y[i], x[t], y[t]);
-    }
-  }
+	for(int i = 0; i < n; i++){
+		out[i] = 0;
+		for(int t = 0; t < n; t++){
+			if(i != t) out[i] += Q(x[i], y[i], x[t], y[t]);
+		}
+	}
 
-  return out;
+	return out;
 }
