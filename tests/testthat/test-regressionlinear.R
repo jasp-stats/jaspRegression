77b257528e3fc7e6b1f5ea154614b6c840c0595c
Alexander Ly <Alexander.Ly.NL@gmail.com>
2021-02-21 14:13:05 +0100

    Regression

--- tests/testthat/test-regressionlinear.R
+++ tests/testthat/test-regressionlinear.R
@@ -7,6 +7,65 @@ context("Linear Regression")
 # - stepwise methods (currently gives an error if I set p entry too high)
 # - plots handle errors
 
+# https://jasp-stats.github.io/jasp-verification-project/regression.html#linear-regression
+test_that("Main table results match R, SPSS, SAS", {
+  options <- jaspTools::analysisOptions("RegressionLinear")
+  options$dependent <- "Y"
+  options$covariates <- "MeanCenteredX"
+  
+  options$modelTerms <- list(
+    list(components="MeanCenteredX", isNuisance=FALSE)
+  )
+  options$descriptives <- TRUE
+  
+  # options$rSquaredChange <- TRUE
+  # options$residualsDurbinWatson <- TRUE
+  results <- jaspTools::runAnalysis("RegressionLinear", "Regression.csv", options)
+  
+  # Main table
+  resultTable <- results$results$modelContainer$collection$modelContainer_summaryTable$data
+  
+  jaspTools::expect_equal_tables(
+    "test"=resultTable,
+    "ref"=list(0, 0, 11.711593246071, 0, "H<unicode>", 0.806436535963374, 0.650339886536607,
+            6.96410449545021, 0.646411121216794, "H<unicode>")
+  )
+  
+  # ANOVA table
+  resultTable <- results$results$modelContainer$collection$modelContainer_anovaTable$data
+  
+  jaspTools::expect_equal_tables(
+    "test"=resultTable,
+    "ref"=list("TRUE", 165.532892294899, 8028.13859583154, 8028.13859583154,
+               "Regression", 1, "H<unicode>", 5.11889396069713e-22, "FALSE",
+               48.4987514235498, 4316.38887669593, "Residual", 89, "H<unicode>",
+               "FALSE", 12344.5274725275, "Total", 90, "H<unicode>")
+  )
+  
+  # Coefficient table
+  resultTable <- results$results$modelContainer$collection$modelContainer_coeffTable$data
+  
+  jaspTools::expect_equal_tables(
+    "test"=resultTable,
+    "ref"=list("FALSE", 1.22770856137111, "H<unicode>", "(Intercept)", 6.22465705469213e-34,
+               19.4143509177338, 23.8351648351648, "TRUE", 0.73003651439274,
+               "H<unicode>", "(Intercept)", 2.54923669436547e-51, 32.6492776255054,
+               23.8351648351648, "FALSE", 0.419657937490671, "H<unicode>",
+               "MeanCenteredX", 5.11889396069709e-22, 0.806436535963375, 12.8659586621013,
+               5.39930167597765)
+  )
+  
+  # Descriptives table
+  resultTable <- results$results$modelContainer$collection$modelContainer_descriptivesTable$data
+  
+  jaspTools::expect_equal_tables(
+    "test"=resultTable,
+    "ref"=list(91, 11.711593246071, 1.22770856137111, 23.8351648351648, "Y",
+               91, 1.74923670777544, 0.183369831659836, 1.14617087951332e-15,
+               "MeanCenteredX")
+  )
+})
+
 test_that("Main table results match", {
   options <- jaspTools::analysisOptions("RegressionLinear")
   options$dependent <- "contNormal"
