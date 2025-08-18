6a0912503d7782d51dfee6a0c9c92321eaf9fd19
coopa33 <dan.yu.h97@gmail.com>
2025-08-18 17:03:58 +0200

    Revert "More accurate name for data extraction function, more compatible caption"
    This reverts commit adc103f65bf732fe18ce4318a6628549d26a57f4.

--- dan_testing/analysis_test.R
+++
@@ -1,5 +0,0 @@
-library("jaspTools")
-setupJaspTools()
-
-options <- analysisOptions('~/Documents/projects/jasp/jaspRegression/dan_testing/iris.jasp')
-runAnalysis(dataset='~/Documents/projects/jasp/jaspRegression/dan_testing/iris.csv', options =  options)
